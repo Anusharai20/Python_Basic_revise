49de77f892d064b6cdff6f1ecef345978a6e1b7b
Anusha <anushaa.rai2058@gmail.com>
2026-06-01 13:11:43 +0545

    basic function

---
+++ functions/function_basics.py
@@ -0,0 +1,25 @@
+# ================================================================================
+# Define a Function
+# function is reusable block of code 
+# runs only when it is called 
+# can return data as a result and helps in avoiding the repetition of the code
+# ================================================================================
+
+# Simple basic codes 
+# Note : functions are defined using def keyword
+# eg: def define_function():
+#       print("defining function syntax")
+    # define_function() -> calling a function
+    
+# example  
+def greet():
+    print("Hello, I am revising Python Programming language from the start!")
+    
+greet()
+
+#  Return values using "return" statement 
+# When a function reaches a return statement, it stops executing and sends the result back
+# If a function doesn't have a return statement, it returns None by default.
+
+# example  
+
