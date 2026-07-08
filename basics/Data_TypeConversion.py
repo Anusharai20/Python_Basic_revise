f9721cd913ef9b7e1c6b6a0b14f01c53d256b462
Anusha <anushaa.rai2058@gmail.com>
2026-07-08 19:38:57 +0545

    some commenting changes

--- basics/Data_TypeConversion.py
+++ basics/Data_TypeConversion.py
@@ -8,11 +8,11 @@ print(type(a))  # Output: <class 'float'>
 print(isinstance(a, int))  # Output: False
 print(isinstance(a, float))  # Output: True
 # -----------------------------------------------------------------------------------------------------------------------------------
-# TYPE OF DATA TYPE Conversion
+# TYPES OF DATA TYPE Conversion
 
 # 1. Implicit data Type Conversion (Type Casting) or Coercion:
 '''Python automatically converts one data type to another without user intervention. This is known as implicit type conversion or coercion. It takes place either during compilation or during run time and is handled directly by Python '''
-
+# Example: When we add an integer and a float, Python automatically converts the integer to a float to avoid losing information.
 a_int = 5
 b_float = 1.0
 c_sum = a_int + b_float
@@ -21,4 +21,23 @@ print(type(c_sum))
 
 '''Why was the float data type chosen for the result? 
 => Because float has a higher precision than int, so Python automatically converts the int to float to avoid losing information.'''
+print('-------------------------------')
+# 2. Exlicit data Type Conversion (Manual):
+'''In explicit type conversion, the user manually converts one data type to another using built-in functions. This is also known as type casting.'''
+# We use the built-in functions like int(), float(), str(), etc to perform explicit type conversion.
+
+# Example: Adding of String and Integer using Explicit Type Conversion
+a_str = "5"
+b_int = 10
+print("Before Conversion: ", type(a_str))  # Output: <class 'str'>
+print("Before Conversion: ", type(b_int))  # Output: <class 'int'>
+
+# explicit type conversion
+a_int = int(a_str)  # Convert string to integer
+print("After Conversion: ", type(a_int))  # Output: <class 'int'>
+
+num_sum = a_int + b_int
+print("Sum: ", num_sum)  # Output: 15
+print("Data type of num_sum: ", type(num_sum))  # Output: <class 'int'>
+
 
